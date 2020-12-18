b396b525e7a77aa8edad5569f1e2554e599bebd7
Dhruv lad <laddhruv@hotmail.com>
2020-12-17 19:34:07 -0500

    Updates

---
+++ precache-manifest.f0e77f9b2a6e0af3fdb902b5e1589a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daefa2047a5fa90c07da4e8ad7697dce",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b0434c703a7ca0a436e8",
+    "url": "/Portfolio/static/css/main.6df308a5.chunk.css"
+  },
+  {
+    "revision": "a0edd5dfe15163f0debe",
+    "url": "/Portfolio/static/js/2.a4fcc986.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/Portfolio/static/js/2.a4fcc986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0434c703a7ca0a436e8",
+    "url": "/Portfolio/static/js/main.3ef6e37d.chunk.js"
+  },
+  {
+    "revision": "ca19561674d2df8f4594",
+    "url": "/Portfolio/static/js/runtime-main.ac1dc205.js"
+  },
+  {
+    "revision": "c5b42acd6496f742bfbc4d1b628a0a2d",
+    "url": "/Portfolio/static/media/background.c5b42acd.jpeg"
+  }
+]);
