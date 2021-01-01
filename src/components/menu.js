2f5e9197f029423ce31e9bdb7a9ca61cdd256333
Dhruv lad <laddhruv@hotmail.com>
2021-01-01 12:21:16 -0500

    added contact form and effect on skills card

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,57 +2,75 @@ import React from "react";
 import { Popover, Menu, Button, Position } from "evergreen-ui";
 import * as LABELS from "../constants/label";
 import { HashLink } from "react-router-hash-link";
+import Contact from "../pages/contact";
 
-const menu = () => {
+const MenuBar = () => {
+  const [state, setState] = React.useState({
+    isShown: false,
+  });
+
+  const handleClostDialog = () => {
+    setState({ isShown: false });
+  };
+
   return (
-    <Popover
-      position={Position.TOP_RIGHT}
-      content={({ close }) => (
-        <Menu>
-          <Menu.Group>
-            <Menu.Item onClick={close}>
-              {" "}
-              <HashLink className="menuTab" smooth to="/">
-                {LABELS.HOME}
-              </HashLink>
-            </Menu.Item>
-            <Menu.Item onClick={close}>
-              <HashLink className="menuTab" smooth to="/#project">
-                {LABELS.PROJECTS}
-              </HashLink>
-            </Menu.Item>
-            <Menu.Item onClick={close}>
-              {" "}
-              <HashLink className="menuTab" smooth to="/#experience">
-                {LABELS.EXPERIENCE}
-              </HashLink>
-            </Menu.Item>
-          </Menu.Group>
-          <Menu.Divider />
-          <Menu.Group>
-            <Menu.Item onClick={close}>
-              {" "}
-              <HashLink className="menuTab" smooth to="/#contact">
-                {LABELS.CONTACT}
-              </HashLink>
-            </Menu.Item>
-          </Menu.Group>
-        </Menu>
-      )}
-    >
-      <Button
-        style={{
-          display: "flex",
-          marginLeft: "auto",
-          marginRight: "3em",
-          marginTop: "3em",
-          backgroundImage: "linear-gradient(to bottom, white, white)",
-        }}
+    <>
+      <Popover
+        position={Position.TOP_RIGHT}
+        content={({ close }) => (
+          <Menu>
+            <Menu.Group>
+              <Menu.Item onClick={close}>
+                {" "}
+                <HashLink className="menuTab" smooth to="/">
+                  {LABELS.HOME}
+                </HashLink>
+              </Menu.Item>
+              <Menu.Item onClick={close}>
+                <HashLink className="menuTab" smooth to="/#project">
+                  {LABELS.PROJECTS}
+                </HashLink>
+              </Menu.Item>
+              <Menu.Item onClick={close}>
+                {" "}
+                <HashLink className="menuTab" smooth to="/#experience">
+                  {LABELS.EXPERIENCE}
+                </HashLink>
+              </Menu.Item>
+            </Menu.Group>
+            <Menu.Divider />
+            <Menu.Group>
+              <Menu.Item onClick={close}>
+                <div
+                  className="menuTab"
+                  onClick={() => setState({ isShown: true })}
+                >
+                  {LABELS.CONTACT}
+                </div>
+              </Menu.Item>
+            </Menu.Group>
+          </Menu>
+        )}
       >
-        <i className="fas fa-bars"></i>
-      </Button>
-    </Popover>
+        <Button
+          style={{
+            display: "flex",
+            marginLeft: "auto",
+            marginRight: "3em",
+            marginTop: "3em",
+            backgroundImage: "linear-gradient(to bottom, white, white)",
+          }}
+        >
+          <i className="fas fa-bars"></i>
+        </Button>
+      </Popover>
+      {state.isShown ? (
+        <Contact isShown={state.isShown} closeDialog={handleClostDialog} />
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
-export default menu;
+export default MenuBar;
